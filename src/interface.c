e089e6833450ba2d3a56acabffae50d729085493
Dark Watchman <space.arseniy@gmail.com>
2021-12-29 16:22:42 +0300

    Main menu stupid prototype

--- src/interface.c
+++ src/interface.c
@@ -2,6 +2,9 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <raylib.h>
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+#undef RAYGUI_IMPLEMENTATION
 
 typedef enum{
   MAIN_MENU,
@@ -25,39 +28,40 @@ void interface_destroy(Interface *interface){
 }
 
 void interface_run(Interface *interface){
-  InitWindow(800, 600, "LABIRINTH SPIRIT");
-  
-  while(!WindowShouldClose()){
-    switch(interface->state){
-      
-    case MAIN_MENU:
-      if(IsKeyPressed(KEY_ENTER)){
-        interface->state = GAMEPLAY;
-      }
-      break;
-    case GAMEPLAY:
-      if(IsKeyPressed(KEY_ENTER)){
-        interface->state = MAIN_MENU;
-      }
-      break;
-    default:
-      assert(0);
-    }
-    
+  InitWindow(800, 600, "LABYRINTH SPIRIT");
+  SetExitKey(KEY_NULL);
+
+  int closer = 0;
+
+  while(!closer){
+
     BeginDrawing();
     switch(interface->state){
     case MAIN_MENU:
       ClearBackground(RAYWHITE);
+      if(GuiButton((Rectangle) {300, 400, 200, 50}, "NEW GAME")){
+        interface->state = GAMEPLAY;
+      }
+      if(GuiButton((Rectangle) {300, 460, 200, 50}, "CONTINUE")){
+        interface->state = GAMEPLAY;
+      }
+      if(GuiButton((Rectangle) {300, 520, 200, 50}, "EXIT")){
+        closer = 1;
+      }
       break;
     case GAMEPLAY:
+      if(IsKeyPressed(KEY_ESCAPE)){
+        interface->state = MAIN_MENU;
+      }
       ClearBackground(BLACK);
       break;
     default:
       assert(0);
     }
     EndDrawing();
+    closer |= WindowShouldClose();
   }
-  
+
   CloseWindow();
   return;
 }
