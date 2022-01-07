39ec6014ec17bd109104d6a078e8591b306f3b45
Dark Watchman <space.arseniy@gmail.com>
2022-01-07 16:40:41 +0300

    small fix

--- src/main.c
+++ src/main.c
@@ -6,8 +6,10 @@
 int main(){
   scm_init_guile();
   engine_register_types();
-  Interface *interface = interface_init();
+  Engine *engine = engine_init();
+  Interface *interface = interface_init(engine);
   interface_run(interface);
   interface_destroy(interface);
+  engine_destroy(engine);
   return 0;
 }
